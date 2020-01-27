4fc34fe2eeccf9f7b7f86b827f9cbe84f772f0e0
Heith Robbins <heith3214@gmail.com>
2020-01-27 15:22:28 -0700

    add all import

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask 
-
+from flask_sqlalchemy import SQLAlchemy 
+from flask_marshmallow import Marshmallow 
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hey Flask"
+    return "Hello Flask"
 
 
 if __name__ == '__main__':
